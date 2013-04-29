fea019224641ebeeb4e2c52a2e75db07ca9aa6e8
Trung Ngo <ndtrung4419@gmail.com>
2013-04-29 17:49:22 +0700

    Add tests for get-accent-char; fix keyword in accent_test.clj

--- bogo/test/bogo/accent_test.clj
+++ bogo/test/bogo/accent_test.clj
@@ -4,14 +4,24 @@
 
 (deftest test-add-accent-string
   (testing "Single-char vowel."
-    (is (= (add-accent-string "a" 2)) "á")
-    (is (= (add-accent-string "nhan" 2)) "nhán")
-    (is (= (add-accent-string "ngang" 2)) "ngáng"))
+    (is (= (add-accent-string "a" :acute)) "á")
+    (is (= (add-accent-string "nhan" :acute)) "nhán")
+    (is (= (add-accent-string "ngang" :acute)) "ngáng"))
   (testing "Double-char vowel."
-    (is (= (add-accent-string "hoa" 1) "hoà"))
-    (is (= (add-accent-string "quy" 1) "quỳ"))
-    (is (= (add-accent-string "mươn" 5) "mượn"))
-    (is (= (add-accent-string "thuôm" 5) "thuộm")))
+    (is (= (add-accent-string "hoa" :grave) "hoà"))
+    (is (= (add-accent-string "quy" :grave) "quỳ"))
+    (is (= (add-accent-string "mươn" :dot) "mượn"))
+    (is (= (add-accent-string "thuôm" :dot) "thuộm")))
   (testing "Triple-char vowel."
-    (is (= (add-accent-string "thiêu" 2) "thiếu"))
-    (is (= (add-accent-string "thuyên" 1) "thuyền"))))
+    (is (= (add-accent-string "thiêu" :acute) "thiếu"))
+    (is (= (add-accent-string "thuyên" :grave) "thuyền"))))
+
+(deftest test-get-accent-char
+  (testing "Simple accent"
+    (is (= (get-accent-char \ả) :hook))
+  (testing "No accent"
+    (is (= (get-accent-char \a) :none))
+    (is (= (get-accent-char \e) :none)))
+  (testing "Consonants"
+    (is (= (get-accent-char \w) :none))
+    (is (= (get-accent-char \n) :none)))))
