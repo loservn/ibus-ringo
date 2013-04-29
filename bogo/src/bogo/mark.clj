8aa5e57be941e0dff0830cdebe67be24c52cbfe0
Trung Ngo <ndtrung4419@gmail.com>
2013-04-29 13:51:12 +0700

    Add simple mark-related functions

---
+++ bogo/src/bogo/mark.clj
@@ -0,0 +1,26 @@
+(ns bogo.mark)
+
+(def mark-afinity {
+  :hat   {:family-a "â"
+          :family-o "ô"}
+  :horn  {:family-o "ơ"
+          :family-u "ư"}
+  :breve {:family-a "ă"}
+  :bar   {:family-d "đ"}})
+
+(defn get-family
+  "doc-string"
+  [chr]
+  (case chr
+    (\a \ă \â) :family-a
+    (\e \ê)    :family-e
+    (\o \ơ \ô) :family-o
+    (\u \ư)    :family-u
+    (\d \đ)    :family-d
+    nil))
+
+
+(defn add-mark-char
+  "Add mark to a single char."
+  [chr mark]
+  (get (get mark-afinity mark) (get-family chr) chr))
